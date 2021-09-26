eccd1c3e06ade72b306290bfc4b0f24bb87e7431
Albert Vallverdu <Albert.vallverdu@reachplc.com>
2021-09-26 11:06:25 +0100

    typescript and scss setup

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,91 @@
 const path = require('path');
+const webpack = require('webpack')
+const { TsconfigPathsPlugin } = require('tsconfig-paths-webpack-plugin')
+const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+const ENV = process.env.ENV || 'development'
+const baseUrl = './src'
+const outputPath = 'build'
 
 module.exports = {
+  mode: ENV,
+
   entry: {
-    app: ['./src/index.js']
+    app: [`${baseUrl}/index.tsx`]
   },
-  module: {
-    rules: [{
-      test: /\.css$/,
-      use: [
-        'style-loader',
-        'css-loader',
-      ],
-    }],
-  },
+  
   output: {
-    path: path.resolve(__dirname, 'build'),
-    publicPath: '/assets/',
+    path: path.resolve(__dirname, outputPath),
+    // publicPath: '/assets/',
     filename: 'bundle.js'
-  }
+  },
+
+  devServer: {
+    contentBase: path.join(__dirname, outputPath),
+    compress: true,
+    port: 9000,
+    hot: true,
+    open: true,
+  },
+
+  devtool: ENV === 'development' ? 'source-map' : undefined,
+
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', '.jsx', '.scss', '.json', 'css'],
+    modules: [path.join(__dirname, 'node_modules')],
+    plugins: [
+      new TsconfigPathsPlugin({
+        configFile: './tsconfig.json',
+        baseUrl,
+      }),
+    ],
+  },
+
+  plugins: [
+    new webpack.WatchIgnorePlugin([/css\.d\.ts$/]),
+    new CleanWebpackPlugin({
+      cleanStaleWebpackAssets: false,
+    }),
+
+    // Webpack plugin that runs TypeScript type checker on a separate process.
+    new ForkTsCheckerWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: './index.html',
+    }),
+    
+  ],
+    module: {
+      rules: [
+        {
+          test: /\.ts(x?)$/,
+          exclude: /node_modules/,
+          use: [
+            {
+              loader: 'ts-loader',
+              options: {
+                transpileOnly: true,
+              },
+            },
+          ],
+        },
+        {
+          test: /\.s[ac]ss$/i,
+          use: [
+            'style-loader',
+            'css-modules-typescript-loader',
+            {
+              loader: 'css-loader',
+              options: {
+                modules: {
+                  localIdentName: '[name]__[local]--[hash:base64:5]',
+                },
+              },
+            },
+            'sass-loader',
+          ],
+        },
+      ],
+    },
 };
