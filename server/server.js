72194a9933b7aeb9f688a99fb91ee101243818d9
byverdu <byverdu@gmail.com>
2021-10-01 20:23:43 +0100

    implement cache files in server

--- server/server.js
+++ server/server.js
@@ -1,14 +1,11 @@
 const http = require('http')
 const { promises } = require('fs')
 const path = require('path')
+const mime = require('mime-types')
 
-const PORT = 9000 || process.env.PORT
+const PORT = process.env.PORT || 9000
 
-const contentTypes = {
-  '.html': 'text/html',
-  '.js': 'text/javascript',
-  '.ico': 'image/x-icon'
-};
+const cacheFilesContent = {}
 
 /**
  * @param {http.IncomingMessage} req
@@ -16,13 +13,19 @@ const contentTypes = {
  */
 function staticFileHandler(req, res) {
   const fileName = req.url === '/' ? path.join(__dirname, './index.html') : path.join(__dirname, req.url)
+  const contentType = mime.contentType(path.extname(fileName))
 
-  const fileExtension = path.extname(fileName);
-
-  if (fileExtension) {
+  if (cacheFilesContent[fileName]) {
+    console.log(`${fileName} is served from cache`)
+    res.writeHead(200, { 'Content-Type': contentType });
+    res.end(cacheFilesContent[fileName])
+  } else {
     promises.readFile(fileName)
       .then(fileContent => {
-        res.writeHead(200, { 'Content-Type': contentTypes[fileExtension] });
+        console.log(`${fileName} has been saved in the cache`)
+
+        cacheFilesContent[fileName] = fileContent;
+        res.writeHead(200, { 'Content-Type': contentType });
         res.end(fileContent)
       })
       .catch(e => {
